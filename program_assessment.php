0ce9492ae5974d5a5530fd722b21f83533b50fd7
Raz Reed <raz@shipstone.com>
2017-10-29 14:44:19 -0400

    Add files via upload

---
+++ program_assessment.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+ob_start();
+require_once 'dbconnect.php';
+
+//uncomment after linking to dashboard, makes sure the user is still logged in
+// if(!isset($_SESSION['user'])) {
+// 	header("Location: login.php");
+// 	exit;
+// }
+
+$error = false;
+$errorText = '';
+
+//set post data to php variables
+//will be more useful later when we validate user entry
+if(isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	// $email = $_POST['email'];
+	$building = $_POST['building'];
+	$date = $_POST['date'];
+	// $time = $_POST['time'];
+	// $location = $_POST['location'];
+	$title = $_POST['title'];
+	$description = $_POST['description'];
+	// $learningObjective = $_POST['learningObjective'];
+	$pre1 = $_POST['pre1'];
+	$pre2 = $_POST['pre2'];
+	$pre3 = $_POST['pre3'];
+	$post1 = $_POST['post1'];
+	$post2 = $_POST['post2'];
+	$post3 = $_POST['post3'];
+	// $budget = $_POST['budget'];
+	//Arrays cannot be put in SQL tables, so they must be converted into a string, 
+	//which can later be decoded back to an array in php using the unserialize() function
+	// $itemNames = serialize($_POST['itemName']);
+	// $itemCosts = serialize($_POST['itemCost']);
+
+	//form validation, makes sure nothiing is empty. Add more later
+	if(empty($name) || empty($building) || empty($date) || empty($title) || empty($description) || empty($pre1) || empty($pre2) || empty($pre3) || empty($post1) || empty($post2) || empty($post3)) {
+		$error = true;
+		$errorText = 'must fill all required fields';
+	}
+	//if no error run sql code, small_program_proposals is the table name, need new table for each form type
+	if(!$error) {
+		$sql = "INSERT INTO program_assessments (name, email, building, `date`, title, description, pre1, pre2, pre3, post1, post2, post3) VALUES ('$name','$email','$building','$date','$title','$description','$pre1','$pre2','$pre3','$post1','$post2','$post3')";
+		if(!mysql_query($sql)) {	//if the sql is bad it prints an error, else it just runs the script
+			die('Error: ' . mysql_error());
+		}
+		header("Location: dashboard.php");	//moves user back to dashboard
+	} else {
+		echo($errorText);
+	}
+}
+//Side note: PHP echo is html code. It can be used to write html using php variables, creating dynamic pages similar to ajax from lab4
+?>
+
+<html>
+	<head>
+		<script type="text/javascript" src="resources/jquery-3.2.1.js"></script>
+	</head>
+	<body>
+		<h2>Staff Post Program Assessment</h2>
+		<!-- Name values are selectors for posting to php -->
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+			<label>Name</label>
+			<input type="text" name="name"/><br>
+			<label>Building</label>
+			<select id="building" name="building">
+				<option value="barton">Barton Hall</option>
+				<option value="barh">BARH</option>
+				<option value="bray">Bray Hall</option>
+				<option value="cary">Cary Hall</option>
+				<option value="hall">Hall Hall</option>
+				<option value="nugent">Nugent Hall</option>
+				<option value="crockett">Crockett Hall</option>
+				<option value="nason">Nason Hall</option>
+				<option value="davison">Davison Hall</option>
+				<option value="sharp">Sharp Hall</option>
+				<option value="warren">Warren Hall</option>
+				<option value="quadI">Quad I</option>
+				<option value="quadII">Quad II</option>
+				<option value="blitman">Blitman Residence Commons</option>
+				<option value="north">North Hall</option>
+				<option value="ecomplex">E-Complex</option>
+				<option value="rahpA">RAHPs A</option>
+				<option value="rahpB">RAHPs B</option>
+				<option value="bryckwyck">Bryckwyck</option>
+			</select><br>
+			<label>Program Date</label>
+			<input type="date" name="date"/><br>
+			<!-- <label>Program Time</label>
+			<input type="time" name="time"/><br>
+			<label>Program Location</label>
+			<input type="text" name="location"/><br> -->
+			<label>Title</label>
+			<input type="text" name="title"/><br>
+			<label>Attendance</label><br>
+			<input type="text" name="attendance"/><br>
+			<!-- <label>Learning objective (one)</label>
+			<input type="text" name="learningObjective"/><br> -->
+			<label>Assessment Questions:</label><br>
+			<input type="text" name="q1"/><br>
+			<input type="text" name="q2"/><br>
+			<input type="text" name="q3"/><br>
+			<label>Percent correct answers for each question for pre assessment:</label><br>
+			<input type="text" name="pre1"/><br>
+			<input type="text" name="pre2"/><br>
+			<input type="text" name="pre3"/><br>
+			<label>Percent correct answers for each question for post assessment:</label><br>
+			<input type="text" name="post1"/><br>
+			<input type="text" name="post2"/><br>
+			<input type="text" name="post3"/><br>
+			<!-- <label>Budget Propsal</label>
+			<input type="number" name="budget"/><br> -->
+			<!-- The [] at the end of itemName and itemCost signify an array type.  -->
+			<!-- <div id="costList">
+			<label>Item Costs</label>
+			<input type="text" name="itemName[]"/>
+			<input type="number" name="itemCost[]"/>
+			<button type="button" id="addItem">Add item</button><br>
+			</div> -->
+			<label>Additional comments/information:</label>
+			<input type="text" name="comments"/><br>
+			<input type="submit" value="Submit" name="submit">
+		</form>
+	</body>
+</html>
---
+++ program_evaluation.php
@@ -0,0 +1,99 @@
+<?php
+session_start();
+ob_start();
+require_once 'dbconnect.php';
+
+//uncomment after linking to dashboard, makes sure the user is still logged in
+// if(!isset($_SESSION['user'])) {
+// 	header("Location: login.php");
+// 	exit;
+// }
+
+$error = false;
+$errorText = '';
+
+//set post data to php variables
+//will be more useful later when we validate user entry
+if(isset($_POST['submit'])) {
+	$major = $_POST['major'];
+	$staff = $_POST['staff'];
+	$building = $_POST['building'];
+	$date = $_POST['date'];
+	$title = $_POST['title'];
+	$category = $_POST['category'];
+	$learned = $_POST['learned'];
+	$enjoyment = $_POST['enjoyment'];
+	$comments = $_POST['comments'];
+
+	//form validation, makes sure nothiing is empty. Add more later
+	if(empty($major) || empty($staff) || empty($building) || empty($date) || empty($title) || empty($category) || empty($learned) || empty($enjoyment)) {
+		$error = true;
+		$errorText = 'must fill all required fields';
+	}
+	//if no error run sql code, small_program_proposals is the table name, need new table for each form type
+	if(!$error) {
+		$sql = "INSERT INTO program_evaluation (major, staff, building, `date`, title, category, learned, enjoyment) VALUES ('$major','$staff','$building','$date','$title','$category','$learned','$enjoyment')";
+		if(!mysql_query($sql)) {	//if the sql is bad it prints an error, else it just runs the script
+			die('Error: ' . mysql_error());
+		}
+		header("Location: dashboard.php");	//moves user back to dashboard
+	} else {
+		echo($errorText);
+	}
+}
+//Side note: PHP echo is html code. It can be used to write html using php variables, creating dynamic pages similar to ajax from lab4
+?>
+
+<html>
+	<head>
+		<script type="text/javascript" src="resources/jquery-3.2.1.js"></script>
+	</head>
+	<body>
+		<h2>Staff Post Program Assessment</h2>
+		<!-- Name values are selectors for posting to php -->
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+			<label>What is your major?</label>
+			<input type="text" name="major"/><br>
+			<label>RA/RD/CM Staff Member(s) that hosted the program:</label>
+			<input type="text" name="staff"/><br>
+			<label>Residence hall that hosted the program:</label>
+			<select id="building" name="building">
+				<option value="barton">Colonie</option>
+				<option value="barh">Bryckwyck</option>
+				<option value="bray">Stackwyck</option>
+				<option value="cary">RAHP A</option>
+				<option value="hall">RAHP B</option>
+				<option value="nugent">BARH</option>
+				<option value="crockett">Barton Hall</option>
+				<option value="nason">Bray Hall</option>
+				<option value="davison">Cary Hall</option>
+				<option value="sharp">Hall Hall</option>
+				<option value="warren">Nason Hall</option>
+				<option value="warren">Davison Hall</option>
+				<option value="warren">Nugent Hall</option>
+				<option value="warren">Warren Hall</option>
+				<option value="warren">Sharp Hall</option>
+				<option value="warren">Polytech Apartments</option>
+				<option value="quadI">Quad I</option>
+				<option value="quadII">Quad II</option>
+				<option value="warren">E Complex</option>
+				<option value="blitman">Blitman</option>
+				<option value="north">City Station West</option>
+				<option value="ecomplex">Campus Partner Program - Multiple Buildings</option>
+			</select><br>
+			<label>Program Date</label>
+			<input type="date" name="date"/><br>
+			<label>Title</label>
+			<input type="text" name="title"/><br>
+			<label>Category</label><br> <!-- change to two radio buttons for social and educational -->
+			<input type="text" name="category"/><br>
+			<label>Tell us a few things you learned from the program:</label><br>
+			<input type="text" name="learned"/><br>
+			<label>Did you enjoy the program?</label> <!-- change to radio buttons 1-5 for enjoyment -->
+			<input type="text" name="enjoyment"/><br>
+			<label>Other feedback?</label>
+			<input type="text" name="comments"/><br>
+			<input type="submit" value="Submit" name="submit">
+		</form>
+	</body>
+</html>
