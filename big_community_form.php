48911a4f7217639288321fcb1e26e256bbe09c3c
aikenj <aikenj@rpi.edu>
2017-11-12 15:36:23 -0500

    Add files via upload

---
+++ big_community_form.php
@@ -0,0 +1,103 @@
+<?php
+
+session_start();
+ob_start();
+require_once 'dbconnect.php';
+
+//uncomment after linking to dashboard, makes sure the user is still logged in
+// if(!isset($_SESSION['user'])) {
+// 	header("Location: login.php");
+// 	exit;
+// }
+
+$error = false;
+$errorText = '';
+
+//set post data to php variables
+//will be more useful later when we validate user entry
+if(isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$building = $_POST['building'];
+	$date = $_POST['date'];
+	$time = $_POST['time'];
+	$location = $_POST['location'];
+	$title = $_POST['title'];
+	$description = $_POST['description'];
+	$learningOutcomes = $_POST['learningOutcomes'];
+	$partnerCollabs = $_POST['partnerCollabs'];
+	$budget = $_POST['budget'];
+	//Arrays cannot be put in SQL tables, so they must be converted into a string, 
+	//which can later be decoded back to an array in php using the unserialize() function
+	$itemNames = serialize($_POST['itemName']);
+	$itemCosts = serialize($_POST['itemCost']);
+	$additional = $_POST['additional'];
+
+	//form validation, makes sure nothiing is empty. Add more later
+
+	//if no error run sql code, small_program_proposals is the table name, need new table for each form type
+	if(!$error) {
+		$sql = "INSERT INTO big_program_proposals (name, building, `date`, `time`, location, title, description, learningOutcomes, budget, itemNames, itemCosts, additional) VALUES ('$name','$building','$date','$time','$location','$title','$description','$learningOutcomes','$budget','$itemNames','$itemCosts', '$additional')";
+		if(!mysql_query($sql)) {	//if the sql is bad it prints an error, else it just runs the script
+			die('Error: ' . mysql_error());
+		}
+		header("Location: dashboard.php");	//moves user back to dashboard
+	} else {
+		echo($errorText);
+	}
+}
+//Side note: PHP echo is html code. It can be used to write html using php variables, creating dynamic pages similar to ajax from lab4
+
+?>
+
+<html>
+	<head>
+		<script type="text/javascript" src="resources/jquery-3.2.1.js"></script>
+	</head>
+	<body>
+		<h2>Big Community Program Proposal</h2>
+		<!-- Name values are selectors for posting to php -->
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+			<label>Name</label>
+			<input type="text" name="name"/><br>
+			<label>Building</label>
+			<input type="text" name="building"/><br>
+			<label>Program Date</label>
+			<input type="date" name="date"/><br>
+			<label>Program Time</label>
+			<input type="time" name="time"/><br>
+			<label>Program Location</label>
+			<input type="text" name="location"/><br>
+			<label>Program Title</label>
+			<input type="text" name="title"/><br>
+			<label>Description</label><br>
+			<textarea name="description" rows="5" cols="50"></textarea><br>
+			<label>Learning Outcomes</label><br>
+			<textarea name="learningOutcomes" rows="4" cols="50"></textarea><br>
+			<label>Partner Collaborations</label><br>
+			<textarea name="partnerCollabs" rows="3" cols="50"></textarea><br>
+			<label>Budget Propsal</label>
+			<input type="number" name="budget"/><br>
+			<!-- The [] at the end of itemName and itemCost signify an array type.  -->
+			<div id="costList">
+			<label>Item Costs</label>
+			<input type="text" name="itemName[]"/>
+			<input type="number" name="itemCost[]"/>
+			<button type="button" id="addItem">Add item</button><br>
+			</div>
+			<lable>Additional Comments/Information</lable><br>
+			<textarea name="additional" rows="4" cols="50"></textarea><br>
+			<input type="submit" value="Submit" name="submit">
+		</form>
+	</body>
+</html>
+<script>
+$(document).ready(function(){
+	var i=1;
+	$('#addItem').click(function(){
+		i++;
+		var html = '<input type="text" name="itemName[]"/>';
+		html += '$<input type="number" name="itemCost[]"/> <br>';
+		$('#costList').append(html);
+	});
+});
+</script>
---
+++ partner_proposal_form.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+ob_start();
+require_once 'dbconnect.php';
+
+//uncomment after linking to dashboard, makes sure the user is still logged in
+// if(!isset($_SESSION['user'])) {
+// 	header("Location: login.php");
+// 	exit;
+// }
+
+$error = false;
+$errorText = '';
+
+//set post data to php variables
+//will be more useful later when we validate user entry
+if(isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$department = $_POST['department'];
+	$date = $_POST['date'];
+	$time = $_POST['time'];
+	$location = $_POST['location'];
+	$classYear = $_POST['classYear'];
+	$curricularExperience = $_POST['curricularExperience'];
+	$description = $_POST['description'];
+	$learningObjectives = $_POST['learningObjectives'];
+	$assessed = $_POST['assessed'];
+	$SLLsupport = $_POST['SLLsupport'];
+	$additional = $_POST['additional'];
+
+	//form validation, makes sure nothiing is empty. Add more later
+
+	//if no error run sql code, small_program_proposals is the table name, need new table for each form type
+	if(!$error) {
+		$sql = "INSERT INTO partner_proposals (name, email, department, `date`, `time`, location, classYear, curricularExperience, description, learningObjectives, assessed, SLLsupport, additional) VALUES ('$name','$email','$department','$date','$time','$location','$classYear','$curricularExperience','$description','$learningObjectives','$assessed','$SLLsupport','$additional')";
+		if(!mysql_query($sql)) {	//if the sql is bad it prints an error, else it just runs the script
+			die('Error: ' . mysql_error());
+		}
+		header("Location: landing.php");	//moves user back to dashboard
+	} else {
+		echo($errorText);
+	}
+}
+//Side note: PHP echo is html code. It can be used to write html using php variables, creating dynamic pages similar to ajax from lab4
+
+?>
+
+
+<html>
+	<head>
+		<script type="text/javascript" src="resources/jquery-3.2.1.js"></script>
+	</head>
+	<body>
+		<h2>Campus Partners Program Proposal</h2>
+		<!-- Name values are selectors for posting to php -->
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+			<label>Your Name</label>
+			<input type="text" name="name"/><br>
+			<label>RPI Email</label>
+			<input type="text" name="email"/><br>
+			<label>Department</label>
+			<input type="text" name="department"/><br>
+			<label>Program Date</label>
+			<input type="date" name="date"/><br>
+			<label>Program Time</label>
+			<input type="time" name="time"/><br><br>
+
+			<label>Program Location</label><br>
+			<input type="radio" name="location" value="EC-4" checked> EC 4 (2 rooms holding 45 people in each, plus outdoor space)<br>
+			<input type="radio" name="location" value="Bray"> Bray Classroom (max 30 people) <br>
+			<input type="radio" name="location" value="Carry"> Carry (max 30 people) <br>
+			<input type="radio" name="location" value="DCC"> DCC Classroom(large programs) <br>
+			<input type="radio" name="location" value="Accademy Hall"> Academy Hall Auditorium (max 75 people) <br><br>
+
+			<label>Class Year Program is Designed For</label>
+			<select name="classYear">
+				<option value="first">First Years</option>
+				<option value="second">Second Years</option>
+				<option value="third">Third Years</option>
+				<option value="fourth">Fourth Years</option>
+				<option value="mix">Mix of years (describe bellow)</option>
+			</select> <br>
+
+			<br><label>What section of the CLASS currecular experience will this program support and enhance?</label>
+			<select name="curricularExperience">
+				<option value="MCGP">Multicultural Sophistication and Global Perspective</option>
+				<option value="HESA">Health and Emotional Self-Awareness</option>
+				<option value="PIR">Problem Identification and Resolution</option>
+				<option value="CCE">Campus Community Engagement</option>
+				<option value="IA">Intelectual Agility</option>
+			</select><br><br>
+
+			<label>Program Description</label><br>
+			<textarea name="description" rows="4" cols="50"></textarea><br>
+			<label>Two to Four Learning Objectives</label><br>
+			<textarea name="learningObjectives" rows="4" cols="50"></textarea><br>
+			<label>How will this program be assesed?</label><br>
+			<textarea name="assessed" rows="4" cols="50"></textarea><br>
+			<lable>Additional Additional Student Living and Learning support needed. Please be specific</lable><br>
+			<textarea name="SLLsupport" rows="4" cols="50"></textarea><br>
+			<lable>Additional Comments/Information</lable><br>
+			<textarea name="additional" rows="4" cols="50"></textarea><br>
+			<input type="submit" value="Submit" name="submit">
+		</form>
+	</body>
+</html>
